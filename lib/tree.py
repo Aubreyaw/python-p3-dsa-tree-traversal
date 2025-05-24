f0c2affd43eb2f8a4f602421432947b1cb03ed0d
aubrey whitford <aubreya.whitford@gmail.com>
2025-05-23 22:04:15 -0400

    Completes lab.

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,16 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+      if not self.root:
+         return None
+      
+      nodes_to_visit = [self.root]
+
+      while nodes_to_visit:
+         node = nodes_to_visit.pop()
+         if node.get('id') == id:
+            return node
+         
+         nodes_to_visit.extend(node.get('children', []))
+
+      return None
